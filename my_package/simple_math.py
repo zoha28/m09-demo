bd5262f746aaf372e13b92a04588ecc6357bd4d6
Zoha Husain <zh2yn@virginia.edu>
2024-10-29 20:18:33 -0400

    Files for package

---
+++ my_package/simple_math.py
@@ -0,0 +1,6 @@
+def add_nums(x, y):
+    "Adds two numbers together"
+    sum = x + y
+    print(sum)
+    return sum
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="Demo",
+    version="0.1",
+    description="A simple description package",
+    url="https://github.com/zoha28/m09-demo",
+    author="Zoha Husain",
+    author_email="zh2yn@virginia.edu",
+    license="MIT",
+    packages=["my_package"]
+)
